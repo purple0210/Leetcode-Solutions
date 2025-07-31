91c4536215261483852e03b288d4c27ff7ee5875
purple0210 <himanshivatsdu@gmail.com>
2025-07-31 08:52:42 +0530

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -6,10 +6,3 @@ class Solution(object):
             if diff in mapping:
                 return [mapping[diff], index]
             mapping[num] = index
-
-if __name__ == "__main__":
-    obj = Solution()
-    nums = [2, 7, 11, 15]   
-    target = 9              
-    result = obj.twoSum(nums, target)
-    print("Result:", result)
